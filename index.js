53a3eed83a71193cdfbb1b35178ee0bd19c21635
william.kalescky@icloud.com <william.kalescky@icloud.com>
2023-04-22 10:07:53 -0700

    cw metric

--- index.js
+++ index.js
@@ -2,39 +2,40 @@ const steamServerQuery = require('steam-server-query');
 const AWS = require('aws-sdk');
 
 AWS.config.update({region: 'REGION'});
+var cw = new AWS.CloudWatch({apiVersion: '2010-08-01'});
 
 steamServerQuery.queryGameServerInfo('localhost:2457').then(infoResponse => {
   console.log(infoResponse);
+  // Create parameters JSON for putMetricData
+  var params = {
+      MetricData: [
+      {
+          MetricName: 'SERVER',
+          Dimensions: [
+          {
+              Name: "WORLD",
+              Value: infoResponse.map
+          },
+          ],
+          Unit: 'Count',
+          Value: infoResponse.players
+      },
+      ],
+      Namespace: 'VALHEIM'
+  };
+
+  cw.putMetricData(params, function(err, data) {
+      if (err) {
+      console.log("Error", err);
+      } else {
+      console.log("Success", JSON.stringify(data));
+      }
+  }); 
 }).catch((err) => {
   console.error(err);
 });
 
 
 // Create CloudWatch service object
-var cw = new AWS.CloudWatch({apiVersion: '2010-08-01'});
 
-// Create parameters JSON for putMetricData
-var params = {
-  MetricData: [
-    {
-      MetricName: 'SERVER',
-      Dimensions: [
-        {
-          Name: "WORLD",
-          Value: infoResponse.map
-        },
-      ],
-      Unit: 'Count',
-      Value: infoResponse.players
-    },
-  ],
-  Namespace: 'VALHEIM'
-};
 
-cw.putMetricData(params, function(err, data) {
-  if (err) {
-    console.log("Error", err);
-  } else {
-    console.log("Success", JSON.stringify(data));
-  }
-}); 
