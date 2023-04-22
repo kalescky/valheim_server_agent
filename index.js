c8598d51027eeeef04da85cb03cade15ea114284
william.kalescky@icloud.com <william.kalescky@icloud.com>
2023-04-22 09:45:58 -0700

    cloudwatch metric boilerplate

--- index.js
+++ index.js
@@ -1,7 +1,41 @@
 import { queryGameServerInfo } from 'steam-server-query';
+var AWS = require('aws-sdk');
 
-queryGameServerInfo('34.209.244.112:2457').then(infoResponse => {
+AWS.config.update({region: 'REGION'});
+
+queryGameServerInfo('localhost:2457').then(infoResponse => {
   console.log(infoResponse);
 }).catch((err) => {
   console.error(err);
+});
+
+
+
+// Create CloudWatch service object
+var cw = new AWS.CloudWatch({apiVersion: '2010-08-01'});
+
+// Create parameters JSON for putMetricData
+var params = {
+  MetricData: [
+    {
+      MetricName: 'SERVER',
+      Dimensions: [
+        {
+          Name: "WORLD",
+          Value: infoResponse.map
+        },
+      ],
+      Unit: 'Count',
+      Value: infoResponse.players
+    },
+  ],
+  Namespace: 'VALHEIM'
+};
+
+cw.putMetricData(params, function(err, data) {
+  if (err) {
+    console.log("Error", err);
+  } else {
+    console.log("Success", JSON.stringify(data));
+  }
 });
